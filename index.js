63b91598e7ca7c7bcd8f50b67bf93fcede4f1b9d
diodes <bbatsios@yahoo.ca>
2025-06-11 06:46:37 -0400

    backend fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
 const dotenv = require('dotenv');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = require('node-fetch');
 
 dotenv.config();
 
